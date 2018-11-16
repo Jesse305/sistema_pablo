a352e74d40b94e1b69a36a34040610d98e3cbfad
Jesse Abreu <jesse.abreu@gdfnet.df.gov.br>
2018-11-16 15:59:53 -0200

    Finaliza layout da tela de registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,14 +5,14 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header bg-success text-light">Registre-se</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">Nome</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
@@ -26,7 +26,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">E-mail</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
@@ -40,7 +40,54 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="altura_cm" class="col-md-4 col-form-label text-md-right">Altura</label>
+
+                            <div class="col-md-3">
+                                <input id="altura_cm" type="text" class="altura form-control{{ $errors->has('altura_cm') ? ' is-invalid' : '' }}" name="altura_cm" value="{{ old('altura_cm') }}" maxlength="3">
+
+                                @if ($errors->has('altura_cm'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('altura_cm') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="peso_kg" class="col-md-4 col-form-label text-md-right">Peso</label>
+
+                            <div class="col-md-3">
+                                <input id="peso_kg" type="text" class="form-control{{ $errors->has('peso_kg') ? ' is-invalid' : '' }}" name="peso_kg" value="{{ old('peso_kg') }}" maxlength="3">
+
+                                @if ($errors->has('peso_kg'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('peso_kg') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="sexo_id" class="col-md-4 col-form-label text-md-right">Sexo</label>
+
+                            <div class="col-md-3">
+                              <div class="form-check-inline">
+                                <label class="form-check-label">
+                                  <input type="radio" class="form-check-input" name="sexo_id" value="1" checked>Masculino
+                                </label>
+                              </div>
+                              <div class="form-check-inline">
+                                <label class="form-check-label">
+                                  <input type="radio" class="form-check-input" name="sexo_id" value="2">Feminino
+                                </label>
+                              </div>
+
+                            </div>
+                        </div>
+
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">Senha</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
@@ -54,7 +101,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirmação</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
@@ -63,8 +110,8 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                <button type="submit" class="btn btn-success">
+                                    Registrar
                                 </button>
                             </div>
                         </div>
--- resources/views/layouts/open.blade.php
+++ resources/views/layouts/open.blade.php
@@ -1,86 +1,78 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>NUTRIÇÃO ATIVA</title>
+  <title>NUTRIÇÃO ATIVA</title>
 
-        <!-- Fonts -->
+  <!-- Fonts -->
 
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-        <!-- Styles -->
-        <style>
+</head>
+<body>
 
-        </style>
-    </head>
-    <body>
+  <nav class="navbar navbar-expand-md bg-success navbar-dark">
+    <!-- Brand -->
+    <a class="navbar-brand" href="{{ route('inicio') }}">Nutrição Ativa</a>
 
-      <nav class="navbar navbar-expand-md bg-success navbar-dark">
-        <!-- Brand -->
-        <a class="navbar-brand" href="{{ route('inicio') }}">Nutrição Ativa</a>
+    <!-- Toggler/collapsibe Button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-        <!-- Toggler/collapsibe Button -->
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+    <!-- Navbar links -->
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('login') }}">Entrar</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('register') }}">Registre-se</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 
-        <!-- Navbar links -->
-        <div class="collapse navbar-collapse" id="collapsibleNavbar">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="{{ route('login') }}">Entrar</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="{{ route('register') }}">Registre-se</a>
-            </li>
-          </ul>
+  <!-- Showcase Slider -->
+  <header id="page-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 m-auto text-center">
+          <h1>Início</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
         </div>
-      </nav>
-
-      <!-- Showcase Slider -->
-      <header id="page-header">
-      	<div class="container">
-      		<div class="row">
-      			<div class="col-md-6 m-auto text-center">
-      				<h1>Início</h1>
-      				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
-      			</div>
-      		</div>
-      	</div>
-      </header>
-
-      <!-- conteúdo aqui -->
-      <div class="container">
-        @yield('content')
       </div>
-      <!-- fim conteúdo -->
+    </div>
+  </header>
 
-      <!-- footer -->
-      <hr>
-      <footer id="main-footer" class="text-center p-4">
-      	<div class="container">
-      		<div class="row">
-      			<div class="col">
-      				<p>Copyright 2018 &copy; Glozzom</p>
-      			</div>
-      		</div>
-      	</div>
-      </footer>
+  <!-- conteúdo aqui -->
+  <div class="container">
+    @yield('content')
+  </div>
+  <!-- fim conteúdo -->
 
-    </body>
-    <script type="text/javascript">
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <!-- footer -->
+  <hr />
+  <footer id="main-footer" class="text-center p-4">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <p>Copyright 2018 &copy; Glozzom</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</body>
 
-    <!-- Popper JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="{{ asset('js/jquery.min.js') }}" charset="utf-8"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<script src="{{ asset('js/jquery.mask.min.js') }}" charset="utf-8"></script>
+<script src="{{ asset('js/app.js') }}" charset="utf-8"></script>
 
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-  </script>
 </html>
